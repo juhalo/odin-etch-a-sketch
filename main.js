5b9e152b7b1f262a3e9b51a9b89addfa36c894af
juhalo <107361221+juhalo@users.noreply.github.com>
2022-08-06 20:52:54 +0300

    Create event listeners for mouseenter and click
    Creates two event listeners: mouseenter which chooses whether or not to
    color a div and click which removes color from a div. Also creates a
    button and its container div and centers the button. Currently the
    button does nothing.

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 let rows = 16;
 const container = document.querySelector('.container');
-console.log(container);
 for (let i = 0; i < rows * rows; i++) {
   const newDiv = document.createElement('div');
   newDiv.style.border = '1px solid'; //For testing; remove later
@@ -8,3 +7,21 @@ for (let i = 0; i < rows * rows; i++) {
 }
 console.log(rows); // For testing; remove later
 container.style.cssText = `grid-template-columns: repeat(${rows}, 1fr);`;
+
+function main() {
+  const divList = document.querySelectorAll('.container > div');
+  divList.forEach((div) => div.addEventListener('mouseenter', colorDiv));
+  document.addEventListener('click', uncolorDiv);
+}
+
+function colorDiv(e) {
+  if (!e.shiftKey) {
+    e.target.classList.add('painted');
+  }
+}
+
+function uncolorDiv(e) {
+  e.target.classList.remove('painted');
+}
+
+main();
